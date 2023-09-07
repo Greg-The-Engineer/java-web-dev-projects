c8180bf00578fc12670c83124e8496d14c88c8b2
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-09-07 10:56:05 -0600

    Chapter 8 Studio Done

--- interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
+++ interfaces/studio/spinning-discs-studio/src/main/java/org/launchcode/Main.java
@@ -6,5 +6,17 @@ public class Main {
         // TODO: Declare and initialize a CD and a DVD object.
 
         // TODO: Call each CD and DVD method to verify that they work as expected.
+
+        // Create a CD object
+        CD myCd = new CD("Music CD", 700, 200);
+
+        // Create a DVD object
+        DVD myDvd = new DVD("Movie DVD", 4700, 1200);
+
+        // Test the spinDisc method for CD and DVD
+        myCd.spinDisc();
+        myDvd.spinDisc();
+
+        // You can perform other operations on myCd and myDvd as needed
     }
 }
