c428e8eca064941dbfa5005ace1070e8a4468e8c
Greg <138933807+sinistergreg@users.noreply.github.com>
2023-09-12 08:48:13 -0600

    Chapter 11 Exercise Done

--- exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
+++ exceptions/exercises/student-grade-exercises/src/main/java/org/launchcode/Main.java
@@ -2,16 +2,35 @@ package org.launchcode;
 
 import java.util.Scanner;
 
-public class ArrayGradebook {
+public class Main {
 
-    public static void main(String[] args) {
-int[] integerArray = {1, 1, 2, 3, 5, 8};
-        for (int num : integerArray) {
-        System.out.println(num);
+        public static void main(String[] args) {
+                int[] integerArray = {1, 1, 2, 3, 5, 8};
+
+                for (int num : integerArray) {
+                        System.out.println(num);
+                }
+
+                for (int num : integerArray) {
+                        if (num % 2 != 0) {
+                                System.out.println(num);
+                        }
+                }
+
+                double result = Divide(10, 5);
+                System.out.println("The result is " + result);
+
+                try {
+                        result = Divide(10, 0);
+                } catch (ArithmeticException e) {
+                        System.out.println("Error: " + e.getMessage());
+                }
         }
 
-        for (int num : integerArray) {
-        if (num % 2 != 0) {
-        System.out.println(num);
+        public static double Divide(int x, int y) throws ArithmeticException {
+                if (y == 0) {
+                        throw new ArithmeticException("Division by zero");
+                }
+                return (double) x / y;
         }
-        }
+}
